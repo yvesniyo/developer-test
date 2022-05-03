3c5c5784b91931d60312b483dfb975c9dcb866a9
Niyobuhungiro Yves <niyobuhungiro.yves@gmail.com>
2022-05-03 09:34:25 +0200

    git init and adding User,Lesson Seeders

---
+++ database/seeders/LessonSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Lesson;
+use Illuminate\Database\Seeder;
+
+class LessonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $lessons = Lesson::factory()
+            ->count(20)
+            ->create();
+    }
+}
